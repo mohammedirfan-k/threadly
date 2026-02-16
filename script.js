168559deb6b3c87d4d0894fc497c69073d8fd874
mohammedirfan-k <mdirfan4929@gmail.com>
2026-02-16 23:35:55 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//Side Navbar
+var sidenavbar = document.querySelector(".side-navbar")
+
+function menuicon(){
+sidenavbar.style.left="0%"
+}
+
+function closeicon(){
+sidenavbar.style.left="-55%"
+}
